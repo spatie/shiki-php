af328c134efa963e2fd90a833d8ffbc85d5dcd7b
Rias <hey@rias.be>
2021-06-28 10:23:39 +0200

    wip

--- src/Shiki.php
+++ src/Shiki.php
@@ -2,6 +2,7 @@
 
 namespace Spatie\ShikiPhp;
 
+use Exception;
 use Illuminate\Support\Collection;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
@@ -11,48 +12,38 @@ class Shiki
     public static function codeToHtml(string $code, string $language = 'php', string $theme = 'nord'): string
     {
         if (! file_exists($theme) && ! self::themes()->contains($theme)) {
-            throw new \Exception("Invalid theme `{$theme}`");
+            throw new Exception("Invalid theme `{$theme}`");
         }
 
         $languages = self::languages();
         $aliases = $languages->pluck('aliases')->flatten();
-        if (! $languages->pluck('id')->merge($aliases)->contains($language)) {
-            throw new \Exception("Invalid language `{$language}`");
+        $languages = $languages->pluck('id')->merge($aliases);
+
+        if (! $languages->contains($language)) {
+            throw new Exception("Invalid language `{$language}`");
         }
 
-        $process = new Process(["node", __DIR__ . '/shiki.js', $code, $language, $theme]);
-        $process->run();
+        return self::callShiki($code, $language, $theme);
+    }
 
+    public static function languages(): Collection
+    {
+        return collect(json_decode(self::callShiki('languages'), true));
+    }
+
+    public static function themes(): Collection
+    {
+        return collect(json_decode(self::callShiki('themes'), true));
+    }
+
+    private static function callShiki(...$arguments): string
+    {
+        $process = new Process(["node", __DIR__ . '/shiki.js', ...$arguments]);
+        $process->run();
         if (! $process->isSuccessful()) {
             throw new ProcessFailedException($process);
         }
 
         return $process->getOutput();
-    }
-
-    public static function languages(): Collection
-    {
-        $process = new Process(["node", __DIR__ . '/shiki.js', 'languages']);
-        $process->run();
-        if (! $process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-
-        $languages = json_decode($process->getOutput(), true);
-
-        return collect($languages);
-    }
-
-    public static function themes(): Collection
-    {
-        $process = new Process(["node", __DIR__ . '/shiki.js', 'themes']);
-        $process->run();
-        if (! $process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-
-        $themes = json_decode($process->getOutput(), true);
-
-        return collect($themes);
     }
 }
